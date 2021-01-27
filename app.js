e49457fd3e0fc4767bee04317f1b45b3eed90868
dragonitaq <kevincharlk@gmail.com>
2021-01-28 00:30:47 +0800

    Improve files & folders structure

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile(`${__dirname}/index.html`);
+  res.status(200).sendFile(`${__dirname}/pages/index.html`);
+});
+
+app.get('/gift', (req, res) => {
+  res.status(200).sendFile(`${__dirname}/pages/gift.html`);
+});
+
+app.get('/delivery', (req, res) => {
+  res.status(200).sendFile(`${__dirname}/pages/delivery.html`);
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).sendFile(`${__dirname}/pages/about.html`);
+});
+
+app.get('/contact', (req, res) => {
+  res.status(200).sendFile(`${__dirname}/pages/contact.html`);
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5555;
 const server = app.listen(port, () => {
-  console.log('Server is live at port 5050');
+  console.log('Server is live at port 5555');
 });
